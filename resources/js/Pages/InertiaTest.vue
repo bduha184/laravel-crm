fd14d6f2653da56f1e4e377b590c007429a74629
bduha184 <bduha_184@yahoo.co.jp>
2023-03-22 14:42:54 +0900

    VUE Linkコンポーネント表示テスト

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+Inertiaテス<br>
+<a href="/">aたぐ</a><br>
+
+<Link href="/">Link</Link>
+<Link :href="route('inertia.index')">名前つきルート確認</Link><br>
+<Link :href="route('inertia.show',{id:1})">ルートパラメータテスト</Link>
+</template>
