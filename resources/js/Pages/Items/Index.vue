77b44055316b6d6ba1c0d850a7dc4c99b52c8590
bduha184 <bduha_184@yahoo.co.jp>
2023-03-24 07:31:42 +0900

    商品登録画面から、商品一覧画面へ遷移

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -27,11 +27,6 @@ defineProps({
                                     class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto"
                                 >
                                 <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">商品登録</Link>
-                                    <button
-                                        class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
-                                    >
-                                        Button
-                                    </button>
                                 </div>
                                 <div
                                     class="lg:w-2/3 w-full mx-auto overflow-auto"
