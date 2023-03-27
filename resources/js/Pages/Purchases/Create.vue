6c876150912b8c037c527c45beb1505868d4840e
bduha184 <bduha_184@yahoo.co.jp>
2023-03-27 22:16:57 +0900

    会員名、購入情報、購入金額、合計金額表示

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -1,25 +1,96 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, router } from "@inertiajs/vue3";
-import { reactive } from "vue";
+import { reactive, onMounted, ref,compile } from "vue";
 import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
+import { getToday } from "@/common";
+import { computed } from "@vue/reactivity";
 
-defineProps({
+const props = defineProps({
     customers: Array,
     items: Array,
-    errors:Array,
 });
+
+const itemList = ref([]);
+
+
+
+onMounted(() => {
+    form.date = getToday();
+    props.items.forEach((item) => {
+        itemList.value.push({
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            quantity: 0,
+        });
+    });
+});
+
+const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 const form = reactive({
-
+    date: null,
+    customer_id: null,
 });
 
-const 
+
+const totalPrice = computed(()=>{
+    let total=0;
+    itemList.value.forEach(item=>{
+        total += item.price*item.quantity;
+    });
+    return total;
+})
 
 </script>
 
 <template>
-    <Head title="購入画面" />
+    日付:<br />
+    <input type="date" name="date" v-model="form.date" /><br />
+    会員名：<br />
+    <select name="customer" v-model="form.customer_id">
+        <option
+            v-for="customer in customers"
+            :key="customer.id"
+            :value="customer.id"
+        >
+            {{ customer.id }}:{{ customer.name }}
+        </option>
+    </select>
+    購入情報：<br />
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>>商品名</th>
+                <th>金額</th>
+                <th>数量</th>
+                <th>小計</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in itemList">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.price }}</td>
+                <td>
+                    <select name="quantity" v-model="item.quantity">
+                        <option v-for="q in quantity" :value="q">
+                            {{ q }}
+                        </option>
+                    </select>
+                </td>
+                <td>
+                    {{ item.price * item.quantity }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    合計金額： {{ totalPrice }}円
+    <!-- <Head title="購入画面" />
 
     <AuthenticatedLayout>
         <template #header>
@@ -103,5 +174,5 @@ const
                 </div>
             </div>
         </div>
-    </AuthenticatedLayout>
+    </AuthenticatedLayout> -->
 </template>
