8b60562b28ad5f7da00ce2fefec4e4969e63d48d
bduha184 <bduha_184@yahoo.co.jp>
2023-03-31 08:25:56 +0900

    日別、月別、年別でそれぞれでグフラ表示

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -13,7 +13,7 @@ onMounted(() => {
 const form = reactive({
     startDate:null,
     endDate:null,
-    type: 'perDay',
+    type: null,
 });
 
 const data = reactive({})
@@ -53,6 +53,14 @@ const getData = async () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="getData">
+                            分析方法<br>
+                            <input type="radio" v-model="form.type" value="perDay" checked>
+                            <span class="mr-4">日別</span>
+                            <input type="radio" v-model="form.type" value="perMonth">
+                            <span class="mr-4">月別</span>
+                            <input type="radio" v-model="form.type" value="perYear">
+                            <span class="mr-4">年別</span>
+                            <br>
                             From:
                             <input
                                 type="date"
